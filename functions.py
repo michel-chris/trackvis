1f0b7a462a8ffad26bf8ee2bc4f3c2e5ffeeb2cd
Chris Michel <c.michel@mailbox.org>
2022-06-19 19:28:21 +0200

    Added sheatmap example, cleaned up comments.

--- functions.py
+++ functions.py
@@ -1,4 +1,5 @@
 import os
+import sys
 import gpxpy
 import folium
 import folium.plugins
@@ -6,14 +7,23 @@ import webbrowser
 import requests
 
 def downloadExample(outputPath):
+    '''
+    Downloads multiple gpx files from Openstreetmap to create an example map.
+    The download links are stored in 'datalinks.txt'.
+    '''
     if outputPath[-1] != '/':
         outputPath = outputPath + '/'
-        
+
+    print('Downloading...')
+
     with open('datalinks.txt') as file:
         for i, line in enumerate(file):
             url = line.rstrip()
             req = requests.get(url)
             open(outputPath + 'track' + str(i) + '.gpx', 'w').write(req.text)
+
+            sys.stdout.write("%d..." % i) # update progress bar
+            sys.stdout.flush()
 
 def readGPX(fileList):
     # read all gpx files on 'path' and append all points to 'tracks'
@@ -51,6 +61,7 @@ def plotMap(
     # add background raster layer
     folium.TileLayer(tiles = tiles, opacity = opacity).add_to(map)
 
+    # draw map depending on user input
     if vectorType == 'polyline':
         for track in tracks:
             folium.PolyLine(track, color = 'red', weight = 2.5, opacity = 0.2).add_to(map)
@@ -83,23 +94,3 @@ def getColormap():
         1.0: '#fcfea4'}
 
     return cmap
-
-
-def progressbar():
-    import time
-    import sys
-
-    toolbar_width = 40
-
-    # setup toolbar
-    sys.stdout.write("[%s]" % (" " * toolbar_width))
-    sys.stdout.flush()
-    sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
-
-    for i in xrange(toolbar_width):
-        time.sleep(0.1) # do real work here
-        # update the bar
-        sys.stdout.write("-")
-        sys.stdout.flush()
-
-    sys.stdout.write("]\n") # this ends the progress bar
--- main.py
+++ main.py
@@ -8,20 +8,25 @@ from functions import *
 
 import os
 
+
+'''
+TODO:
+- write readme
+- only download example files on first run
+- add other colormaps
+- add configuration file (yaml??)
+- add zoom_start option (any way to programmetically calculate an appropriate value?)
+'''
+
+
 # quick start guide for folium maps:
 # https://python-visualization.github.io/folium/quickstart.html
 
+# set data path with gpx files
+dataPath = os.path.abspath('gpx')
 
-dataPath = os.path.abspath('example')
-
+# download example gpx files from Openstreetmap
 downloadExample(dataPath)
-
-
-# Use polyline if:
-# - you have a low number of gnss points per distance
-# - you need a more responsive map since you have a large number of tracks
-# adapt color, weight (line thickness) and opacity to your liking
-# (I suggest weight=2.5, opacity=0.2)
 
 # folder path with all gpx tracks
 fileList = [os.path.join(dataPath, f) for f in os.listdir(dataPath)]
@@ -31,14 +36,26 @@ fileList = [os.path.join(dataPath, f) for f in os.listdir(dataPath)]
 # tracks[i][j][1] == i-th track, j-th point, longitude
 tracks = readGPX(fileList)
 
-plotMap(tracks, vectorType = 'polyline', tiles = 'Stamen Terrain', outputFile = dataPath + '/city.html')
+
+# Use polyline if:
+# - you have a low number of gnss points per distance
+# - you need a more responsive map since you have a large number of tracks
+#
+# adapt color, weight (line thickness) and opacity to your liking
+# (I suggest weight = 2.5, opacity = 0.2)
+
+plotMap(tracks, vectorType = 'polyline', tiles = 'Stamen Terrain', outputFile = dataPath + '/exampleLines.html')
 
 
 # Use heatmap if:
-# - polyline looks ugly
-# - you have normal point density (every 3 meters?)
+# - you have many overlapping tracks, which cannot be displayed by polyline
+#   e.g. opacity = 0.2 --> max. number of lines is 5
+# - you have sufficiently high point density (every 3 meters??)
 # - you don't care about gaps in your lines
-# - you have a powerful pc in case of a large number of tracks
-# adapt radius and blur to your liking (I suggest radius=2, blur=3)
+# - you don't care about unresponsive maps or have a powerful pc
+#   (in the case of a large number of tracks)
+#
+# adapt radius and blur to your liking (I suggest radius = 2, blur = 3)
 
+plotMap(tracks, vectorType = 'heatmap', tiles = 'Stamen Terrain', outputFile = dataPath + '/exampleHeat.html')
 
